f3f2681546b8e5e9a6bf7563d8aeede89479d261
Ruben Freddy Quea Jacho <qjcoders@gmail.com>
2023-11-07 10:27:17 -0500

    LAB10-PMA-Process

---
+++ QueaSnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  QueaSnapchat
+//
+//  Created by Ruben Freddy Quea Jacho on 7/11/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class iniciarSesionViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func IniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){
+            (user, error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil {
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+}
+
