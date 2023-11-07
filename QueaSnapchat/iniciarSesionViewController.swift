8a80f0c32f93001739026657a251ec9c8ddefcde
fquea15 <qjcoders@gmail.com>
2023-11-07 18:04:35 -0500

    test

--- QueaSnapchat/iniciarSesionViewController.swift
+++ QueaSnapchat/iniciarSesionViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import FirebaseAuth
+import GoogleSignIn
+import Firebase
 
-class iniciarSesionViewController: UIViewController {
+
+
+class iniciarSesionViewController: UIViewController, GIDSignIn{
 
     
     @IBOutlet weak var emailTextField: UITextField!
@@ -30,6 +34,45 @@ class iniciarSesionViewController: UIViewController {
             }
         }
     }
+    @IBAction func InciarSesionGoogleTapped(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Crea el objeto de configuración de inicio de sesión de Google.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // Inicia el flujo de inicio de sesión de Google.
+            GIDSignIn.sharedInstance().signIn(withPresenting: self) { [unowned self] (result: GIDGoogleUser?, error: Error?) in
+                guard let result = result else {
+                    
+                    return
+                }
+
+            guard let user = result?.user,
+                let idToken = user.authentication.idToken
+            else {
+                // Maneja el caso donde no se obtuvo el usuario o el token de ID.
+                print("No se pudo obtener el usuario o el token de ID.")
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: user.authentication.accessToken)
+
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    // Maneja el error al autenticar con Firebase.
+                    print("Error al autenticar con Firebase: \(error.localizedDescription)")
+                    return
+                }
+
+                // Si no hay errores, el inicio de sesión con Google fue exitoso.
+                print("Inicio de sesión con Google exitoso!")
+            }
+        }
+    }
     
 }
+
+
 
